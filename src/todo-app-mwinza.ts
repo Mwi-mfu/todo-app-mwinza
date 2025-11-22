f6f019ec7b187a4708845690429b4e911d0f8e2f
Mwi-mfu <mfunemwinza@gmail.com>
2025-11-22 12:19:34 +0200

    Second commit. Added user input

--- src/todo-app-mwinza.ts
+++ src/todo-app-mwinza.ts
@@ -1,6 +1,13 @@
 import { JSONFilePreset } from 'lowdb/node';
+import readline from 'readline/promises';
 
-const list = await JSONFilePreset('tasks.json', { tasks: [] })
+const db = await JSONFilePreset('tasks.json', { tasks: [] });
+
+const rl = await readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
 type taskBody = {
   name: string,
   completed: boolean
@@ -9,25 +16,32 @@ type taskBody = {
 let active: boolean = true;
 
 
-function main(): void {
-  while (active){
+async function main() {
+  await db.read;
+  const actions: Array<string> = ['Add task', 'View tasks', 'Mark task as completed', 'Delete task', 'Exit'];
 
+  while (active){
+    console.log("==========================================")
+    console.log("                TO DO LIST")
+    console.log("==========================================\n\n")
+
+    printList(actions);
   }
 }
 
-function addTask(): void {
+async function addTask() {
 
 }
 
-function viewTasks(): void {
+async function viewTasks() {
 
 }
 
-function markAsComplete(): void {
+async function markAsComplete() {
 
 }
 
-function deleteTask(): void {
+async function deleteTask() {
 
 }
 
@@ -35,3 +49,11 @@ function exit(): void {
   active = false;
 }
 
+function printList(list: Array<string>): void {
+  let index: number = 1;
+  for (let element of list) {
+    console.log(`${index}. ${element}`);
+  }
+}
+
+main();
